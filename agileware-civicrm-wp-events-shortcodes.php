69db7dcdb13b7b9949cb998298bd8c9001bc8a3c
Justin Freeman <justin@agileware.com.au>
2017-06-02 07:31:28 +1000

    APPNFP-5 SUP-694 SUP-1172 Reformat code

--- agileware-civicrm-wp-events-shortcodes.php
+++ agileware-civicrm-wp-events-shortcodes.php
@@ -3,75 +3,76 @@
 // Upcoming Events
 // =============================================================================
 
-function agileware_civicrm_wp_events_upcoming_events( $atts ) {
-  extract( shortcode_atts( array(
-    'id'          => '',
-    'class'       => '',
-    'style'       => '',
-    'type'        => 'aa-event',
-    'count'       => '',
-    'category'    => '',
-    'offset'      => '',
-    'orientation' => '',
-    'no_sticky'   => '',
-    'no_image'    => '',
-    'fade'        => ''
-  ), $atts, 'aa_upcoming_events' ) );
+function agileware_civicrm_wp_events_upcoming_events($atts)
+{
+    extract(shortcode_atts(array(
+        'id' => '',
+        'class' => '',
+        'style' => '',
+        'type' => 'aa-event',
+        'count' => '',
+        'category' => '',
+        'offset' => '',
+        'orientation' => '',
+        'no_sticky' => '',
+        'no_image' => '',
+        'fade' => ''
+    ), $atts, 'aa_upcoming_events'));
 
 
-  $type = 'aa-event';
+    $type = 'aa-event';
 
-  $id            = ( $id          != ''          ) ? 'id="' . esc_attr( $id ) . '"' : '';
-  $class         = ( $class       != ''          ) ? 'x-recent-posts cf ' . esc_attr( $class ) : 'x-recent-posts cf';
-  $style         = ( $style       != ''          ) ? 'style="' . $style . '"' : '';
-  $count         = ( $count       != ''          ) ? $count : 3;
-  $category      = ( $category    != ''          ) ? $category : '';
-  $category_type = ( $type        == 'post'      ) ? 'category_name' : 'portfolio-category';
-  $offset        = ( $offset      != ''          ) ? $offset : 0;
-  $orientation   = ( $orientation != ''          ) ? ' ' . $orientation : ' horizontal';
-  $no_sticky     = ( $no_sticky   == 'true'      );
-  $no_image      = ( $no_image    == 'true'      ) ? $no_image : '';
-  $fade          = ( $fade        == 'true'      ) ? $fade : 'false';
+    $id = ($id != '') ? 'id="' . esc_attr($id) . '"' : '';
+    $class = ($class != '') ? 'x-recent-posts cf ' . esc_attr($class) : 'x-recent-posts cf';
+    $style = ($style != '') ? 'style="' . $style . '"' : '';
+    $count = ($count != '') ? $count : 3;
+    $category = ($category != '') ? $category : '';
+    $category_type = ($type == 'post') ? 'category_name' : 'portfolio-category';
+    $offset = ($offset != '') ? $offset : 0;
+    $orientation = ($orientation != '') ? ' ' . $orientation : ' horizontal';
+    $no_sticky = ($no_sticky == 'true');
+    $no_image = ($no_image == 'true') ? $no_image : '';
+    $fade = ($fade == 'true') ? $fade : 'false';
 
-  $js_params = array(
-    'fade' => ( $fade == 'true' )
-  );
+    $js_params = array(
+        'fade' => ($fade == 'true')
+    );
 
-  $data = cs_generate_data_attributes( 'upcoming_events', $js_params );
+    $data = cs_generate_data_attributes('upcoming_events', $js_params);
 
-  $output = "<div {$id} class=\"{$class}{$orientation}\" {$style} {$data} data-fade=\"{$fade}\" >";
+    $output = "<div {$id} class=\"{$class}{$orientation}\" {$style} {$data} data-fade=\"{$fade}\" >";
 
-    $q = new WP_Query( array(
-      'meta_key'            => 'aa-event-start',
-      'orderby'             => 'meta_value',
-      'order'               => 'ASC',
-      'post_type'           => "{$type}",
-      'posts_per_page'      => "{$count}",
-      'offset'              => "{$offset}",
-      "{$category_type}"    => "{$category}",
-      'ignore_sticky_posts' => $no_sticky,
-      'meta_query' => array(
-        array(
-          'key' => 'aa-event-end',
-          'value' => date('Y-m-d H:i:s'),
-          'compare' => '>='
+    $q = new WP_Query(array(
+        'meta_key' => 'aa-event-start',
+        'orderby' => 'meta_value',
+        'order' => 'ASC',
+        'post_type' => "{$type}",
+        'posts_per_page' => "{$count}",
+        'offset' => "{$offset}",
+        "{$category_type}" => "{$category}",
+        'ignore_sticky_posts' => $no_sticky,
+        'meta_query' => array(
+            array(
+                'key' => 'aa-event-end',
+                'value' => date('Y-m-d H:i:s'),
+                'compare' => '>='
+            )
         )
-      )
-    ) );
+    ));
 
-    if ( $q->have_posts() ) : while ( $q->have_posts() ) : $q->the_post();
+    if ($q->have_posts()) : while ($q->have_posts()) : $q->the_post();
 
-      if ( $no_image == 'true' ) {
-        $image_output       = '';
-        $image_output_class = 'no-image';
-      } else {
-        $image              = wp_get_attachment_image_src( get_post_thumbnail_id(), 'entry-cropped' );
-        $bg_image           = ( $image[0] != '' ) ? ' style="background-image: url(' . $image[0] . ');"' : '';
-        $image_output       = '<div class="x-recent-posts-img"' . $bg_image . '></div>';
-        $image_output_class = 'with-image';
-      }
+        if ($no_image == 'true') {
+            $image_output = '';
+            $image_output_class = 'no-image';
+        } else {
+            $image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'entry-cropped');
+            $bg_image = ($image[0] != '') ? ' style="background-image: url(' . $image[0] . ');"' : '';
+            $image_output = '<div class="x-recent-posts-img"' . $bg_image . '></div>';
+            $image_output_class = 'with-image';
+        }
 
-      $event_start  = date('j F Y', strtotime(get_post_meta( get_the_ID(), 'aa-event-start', true )));
+        $event_start = date('j F Y', strtotime(get_post_meta(get_the_ID(), 'aa-event-start', true)));
 
         $output .= '<a class="x-recent-post' . $count . ' ' . $image_output_class . '" href="' . get_permalink(get_the_ID()) . '" title="' . esc_attr(sprintf(__('Permalink to: "%s"', 'cornerstone'), the_title_attribute('echo=0'))) . '">'
             . '<article id="post-' . get_the_ID() . '" class="' . implode(' ', get_post_class()) . '">'
@@ -85,11 +86,12 @@ function agileware_civicrm_wp_events_upcoming_events( $atts ) {
             . '</article>'
             . '</a>';
 
-    endwhile; endif; wp_reset_postdata();
+    endwhile; endif;
+    wp_reset_postdata();
 
-  $output .= '</div>';
+    $output .= '</div>';
 
-  return $output;
+    return $output;
 }
 
-add_shortcode( 'aa_upcoming_events', 'agileware_civicrm_wp_events_upcoming_events' );
+add_shortcode('aa_upcoming_events', 'agileware_civicrm_wp_events_upcoming_events');
